ebac8adcfcb54d9508a1dee050bb25bdd779859b
android0206 <gromovandrej94@gmail.com>
2024-11-04 22:06:51 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,15 +1,18 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(group1, group2, arg=','):
-    set1 = set(group1.split(arg))
-    list2 = group2.split(arg)
-    both = list(set1.intersection(list2))
-    both.sort()
-    return both
-
+    set_for_intersection = set(group1.split(arg))
+    # Используем set для метода intersection,
+    # разделяем строку по разделителю и записываем в список, затем во множество
+    list_of_group2 = group2.split(arg)
+    # Разделяем строку по разделителю и записываем в список
+    both_participants = list(set_for_intersection.intersection(list_of_group2))
+    # Находим общих участников
+    both_participants.sort()
+    # Алфавитный порядок
+    return both_participants
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-divide = '|'
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-find_common_participants(participants_first_group, participants_second_group, divide)
+print(find_common_participants(participants_first_group, participants_second_group, arg='|'))
